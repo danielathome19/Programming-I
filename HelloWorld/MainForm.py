0b61c8f000bc640abd9494cee2bfc7c712e19440
Daniel James Szelogowski <dszelogowski@gmail.com>
2023-09-26 13:44:38 -0500

    Update 9/26

---
+++ HelloWorld/MainForm.py
@@ -0,0 +1,64 @@
+﻿import System.Drawing
+import System.Windows.Forms
+
+from System.Drawing import *
+from System.Windows.Forms import *
+
+class MainForm(Form):
+	def __init__(self):
+		self.InitializeComponent()
+	
+	def InitializeComponent(self):
+		self._label1 = System.Windows.Forms.Label()
+		self._button1 = System.Windows.Forms.Button()
+		self._button2 = System.Windows.Forms.Button()
+		self.SuspendLayout()
+		# 
+		# label1
+		# 
+		self._label1.BackColor = System.Drawing.SystemColors.ActiveCaption
+		self._label1.Font = System.Drawing.Font("Microsoft Sans Serif", 26, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
+		self._label1.Location = System.Drawing.Point(158, 54)
+		self._label1.Name = "label1"
+		self._label1.Size = System.Drawing.Size(456, 265)
+		self._label1.TabIndex = 0
+		self._label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+		# 
+		# button1
+		# 
+		self._button1.Font = System.Drawing.Font("Microsoft Sans Serif", 22, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
+		self._button1.Location = System.Drawing.Point(158, 374)
+		self._button1.Name = "button1"
+		self._button1.Size = System.Drawing.Size(175, 121)
+		self._button1.TabIndex = 1
+		self._button1.Text = "Show"
+		self._button1.UseVisualStyleBackColor = True
+		self._button1.Click += self.Button1Click
+		# 
+		# button2
+		# 
+		self._button2.Font = System.Drawing.Font("Microsoft Sans Serif", 22, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
+		self._button2.Location = System.Drawing.Point(428, 374)
+		self._button2.Name = "button2"
+		self._button2.Size = System.Drawing.Size(175, 121)
+		self._button2.TabIndex = 2
+		self._button2.Text = "Clear"
+		self._button2.UseVisualStyleBackColor = True
+		self._button2.Click += self.Button2Click
+		# 
+		# MainForm
+		# 
+		self.ClientSize = System.Drawing.Size(785, 605)
+		self.Controls.Add(self._button2)
+		self.Controls.Add(self._button1)
+		self.Controls.Add(self._label1)
+		self.Name = "MainForm"
+		self.Text = "HelloWorld"
+		self.ResumeLayout(False)
+
+
+	def Button1Click(self, sender, e):
+		self._label1.Text = "Hello, world!"
+
+	def Button2Click(self, sender, e):
+		self._label1.Text = ""
---
+++ MyName/Program.rb
@@ -0,0 +1,10 @@
+﻿require "mscorlib"
+require "System.Windows.Forms, Version=2.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089"
+require "System.Drawing, Version=2.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a"
+require "MainForm"
+
+Application = System::Windows::Forms::Application
+
+Application.EnableVisualStyles()
+form = MainForm.new()
+Application.Run(form)
