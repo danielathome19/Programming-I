69d86f1e6f4f87a135d10596c453ca06a8586c0a
Daniel James Szelogowski <dszelogowski@gmail.com>
2023-10-16 10:31:01 -0500

    Update 10/16

---
+++ Prog54a/MainForm.py
@@ -0,0 +1,193 @@
+﻿import System.Drawing
+import System.Windows.Forms
+
+from System.Drawing import *
+from System.Windows.Forms import *
+
+class MainForm(Form):
+	def __init__(self):
+		self.InitializeComponent()
+	
+	def InitializeComponent(self):
+		self._comboBox1 = System.Windows.Forms.ComboBox()
+		self._label1 = System.Windows.Forms.Label()
+		self._button1 = System.Windows.Forms.Button()
+		self._button2 = System.Windows.Forms.Button()
+		self._button3 = System.Windows.Forms.Button()
+		self._label2 = System.Windows.Forms.Label()
+		self._label3 = System.Windows.Forms.Label()
+		self._label4 = System.Windows.Forms.Label()
+		self._label5 = System.Windows.Forms.Label()
+		self._label6 = System.Windows.Forms.Label()
+		self._label7 = System.Windows.Forms.Label()
+		self.SuspendLayout()
+		# 
+		# comboBox1
+		# 
+		self._comboBox1.Font = System.Drawing.Font("Microsoft Sans Serif", 18, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
+		self._comboBox1.FormattingEnabled = True
+		self._comboBox1.Items.AddRange(System.Array[System.Object](
+			["1970 VW Bug",
+			"1979 Firebird"]))
+		self._comboBox1.Location = System.Drawing.Point(235, 26)
+		self._comboBox1.Name = "comboBox1"
+		self._comboBox1.Size = System.Drawing.Size(460, 48)
+		self._comboBox1.TabIndex = 0
+		# 
+		# label1
+		# 
+		self._label1.Font = System.Drawing.Font("Microsoft Sans Serif", 18, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
+		self._label1.Location = System.Drawing.Point(12, 26)
+		self._label1.Name = "label1"
+		self._label1.Size = System.Drawing.Size(217, 48)
+		self._label1.TabIndex = 1
+		self._label1.Text = "Pick a car:"
+		# 
+		# button1
+		# 
+		self._button1.BackColor = System.Drawing.Color.FromArgb(192, 64, 0)
+		self._button1.Font = System.Drawing.Font("Microsoft Sans Serif", 18, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
+		self._button1.ForeColor = System.Drawing.SystemColors.ButtonHighlight
+		self._button1.Location = System.Drawing.Point(12, 442)
+		self._button1.Name = "button1"
+		self._button1.Size = System.Drawing.Size(200, 90)
+		self._button1.TabIndex = 2
+		self._button1.Text = "Calculate"
+		self._button1.UseVisualStyleBackColor = False
+		self._button1.Click += self.Button1Click
+		# 
+		# button2
+		# 
+		self._button2.BackColor = System.Drawing.Color.FromArgb(192, 64, 0)
+		self._button2.Font = System.Drawing.Font("Microsoft Sans Serif", 18, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
+		self._button2.ForeColor = System.Drawing.SystemColors.ButtonHighlight
+		self._button2.Location = System.Drawing.Point(251, 442)
+		self._button2.Name = "button2"
+		self._button2.Size = System.Drawing.Size(200, 90)
+		self._button2.TabIndex = 3
+		self._button2.Text = "Clear"
+		self._button2.UseVisualStyleBackColor = False
+		# 
+		# button3
+		# 
+		self._button3.BackColor = System.Drawing.Color.FromArgb(192, 64, 0)
+		self._button3.Font = System.Drawing.Font("Microsoft Sans Serif", 18, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
+		self._button3.ForeColor = System.Drawing.SystemColors.ButtonHighlight
+		self._button3.Location = System.Drawing.Point(495, 442)
+		self._button3.Name = "button3"
+		self._button3.Size = System.Drawing.Size(200, 90)
+		self._button3.TabIndex = 4
+		self._button3.Text = "Exit"
+		self._button3.UseVisualStyleBackColor = False
+		# 
+		# label2
+		# 
+		self._label2.BackColor = System.Drawing.Color.FromArgb(255, 128, 0)
+		self._label2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+		self._label2.Font = System.Drawing.Font("Microsoft Sans Serif", 18, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
+		self._label2.Location = System.Drawing.Point(12, 109)
+		self._label2.Name = "label2"
+		self._label2.Size = System.Drawing.Size(217, 80)
+		self._label2.TabIndex = 5
+		self._label2.Text = "Miles:"
+		self._label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+		# 
+		# label3
+		# 
+		self._label3.BackColor = System.Drawing.Color.FromArgb(255, 128, 0)
+		self._label3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+		self._label3.Font = System.Drawing.Font("Microsoft Sans Serif", 18, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
+		self._label3.Location = System.Drawing.Point(12, 220)
+		self._label3.Name = "label3"
+		self._label3.Size = System.Drawing.Size(217, 80)
+		self._label3.TabIndex = 6
+		self._label3.Text = "Gallons:"
+		self._label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+		# 
+		# label4
+		# 
+		self._label4.BackColor = System.Drawing.Color.FromArgb(255, 128, 0)
+		self._label4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+		self._label4.Font = System.Drawing.Font("Microsoft Sans Serif", 18, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
+		self._label4.Location = System.Drawing.Point(12, 338)
+		self._label4.Name = "label4"
+		self._label4.Size = System.Drawing.Size(217, 80)
+		self._label4.TabIndex = 7
+		self._label4.Text = "MPG:"
+		self._label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+		# 
+		# label5
+		# 
+		self._label5.BackColor = System.Drawing.Color.FromArgb(255, 128, 0)
+		self._label5.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+		self._label5.Font = System.Drawing.Font("Microsoft Sans Serif", 18, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
+		self._label5.Location = System.Drawing.Point(251, 109)
+		self._label5.Name = "label5"
+		self._label5.Size = System.Drawing.Size(444, 80)
+		self._label5.TabIndex = 10
+		self._label5.Text = " "
+		self._label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+		# 
+		# label6
+		# 
+		self._label6.BackColor = System.Drawing.Color.FromArgb(255, 128, 0)
+		self._label6.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+		self._label6.Font = System.Drawing.Font("Microsoft Sans Serif", 18, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
+		self._label6.Location = System.Drawing.Point(251, 220)
+		self._label6.Name = "label6"
+		self._label6.Size = System.Drawing.Size(444, 80)
+		self._label6.TabIndex = 9
+		self._label6.Text = " "
+		self._label6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+		# 
+		# label7
+		# 
+		self._label7.BackColor = System.Drawing.Color.FromArgb(255, 128, 0)
+		self._label7.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+		self._label7.Font = System.Drawing.Font("Microsoft Sans Serif", 18, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
+		self._label7.Location = System.Drawing.Point(251, 338)
+		self._label7.Name = "label7"
+		self._label7.Size = System.Drawing.Size(444, 80)
+		self._label7.TabIndex = 8
+		self._label7.Text = " "
+		self._label7.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+		# 
+		# MainForm
+		# 
+		self.BackColor = System.Drawing.Color.FromArgb(255, 192, 128)
+		self.ClientSize = System.Drawing.Size(707, 556)
+		self.Controls.Add(self._label5)
+		self.Controls.Add(self._label6)
+		self.Controls.Add(self._label7)
+		self.Controls.Add(self._label4)
+		self.Controls.Add(self._label3)
+		self.Controls.Add(self._label2)
+		self.Controls.Add(self._button3)
+		self.Controls.Add(self._button2)
+		self.Controls.Add(self._button1)
+		self.Controls.Add(self._label1)
+		self.Controls.Add(self._comboBox1)
+		self.Name = "MainForm"
+		self.Text = "Prog54a"
+		self.ResumeLayout(False)
+
+
+	def Button1Click(self, sender, e):
+		miles = 0
+		gallons = 0
+		mpg = 0
+		car = self._comboBox1.Text
+		
+		if car == "1970 VW Bug":
+			miles = 286
+			gallons = 9
+		# elif car == "second car":
+		# 	miles = ...
+		# 	gallons = ...
+		
+		# Cast one or both as float to do real division
+		mpg = miles / float(gallons)
+		mpg = round(mpg, 10)
+		
+		
+		self._label7.Text = str(mpg)
