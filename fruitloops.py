1a9ca73f9352360aee016ddfcb1723cf018cc512
Daniel James Szelogowski <dszelogowski@gmail.com>
2023-10-24 10:06:09 -0500

    Update 10/24

---
+++ fruitloops.py
@@ -0,0 +1,17 @@
+colors = ["red", "green", "blue"]
+print(colors)
+print(colors[1])
+
+fruits = ["apple", "banana", "orange", "kiwi", "mango"]
+print(len(fruits))
+for index in range(len(fruits)):
+  print(fruits[index])
+
+print()
+for fruit in fruits:
+  print(fruit)  # For-each loop
+
+# lastfruit = fruits[len(fruits) - 1]
+lastfruit = fruits[-1]
+print(lastfruit)
+  
