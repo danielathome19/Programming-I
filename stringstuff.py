73ea95cdf564187c6f0dd9d37d0c134b4a5cbc07
Daniel James Szelogowski <dszelogowski@gmail.com>
2023-11-06 10:22:26 -0600

    Update 11/6

---
+++ stringstuff.py
@@ -0,0 +1,10 @@
+thing = "HELLO world HERE is SOME text"
+print(thing.lower())
+print(thing.upper())
+print(thing.isdigit())
+locworld = thing.find("world")
+print(locworld)  # indexOf
+print(thing.count('E'))
+print(thing[::-1])
+print(thing.split(' '))
+print("Hello" * 5)
