be6b83e31d12466a8bff3414bb9a6c4207c42d81
Daniel James Szelogowski <dszelogowski@gmail.com>
2024-03-27 10:18:55 -0500

    Update 3/27

---
+++ Prog162b/MainForm.py
@@ -0,0 +1,84 @@
+﻿import System.Drawing
+import System.Windows.Forms
+
+from System.Drawing import *
+from System.Windows.Forms import *
+
+class MainForm(Form):
+	def __init__(self):
+		self.InitializeComponent()
+	
+	def InitializeComponent(self):
+		self._listBox1 = System.Windows.Forms.ListBox()
+		self._button1 = System.Windows.Forms.Button()
+		self._button2 = System.Windows.Forms.Button()
+		self._button3 = System.Windows.Forms.Button()
+		self.SuspendLayout()
+		# 
+		# listBox1
+		# 
+		self._listBox1.Font = System.Drawing.Font("Microsoft Sans Serif", 16, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
+		self._listBox1.FormattingEnabled = True
+		self._listBox1.ItemHeight = 37
+		self._listBox1.Location = System.Drawing.Point(12, 12)
+		self._listBox1.Name = "listBox1"
+		self._listBox1.Size = System.Drawing.Size(532, 337)
+		self._listBox1.TabIndex = 0
+		# 
+		# button1
+		# 
+		self._button1.Font = System.Drawing.Font("Microsoft Sans Serif", 16, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
+		self._button1.Location = System.Drawing.Point(12, 355)
+		self._button1.Name = "button1"
+		self._button1.Size = System.Drawing.Size(160, 88)
+		self._button1.TabIndex = 1
+		self._button1.Text = "Calculate"
+		self._button1.UseVisualStyleBackColor = True
+		self._button1.Click += self.Button1Click
+		# 
+		# button2
+		# 
+		self._button2.Font = System.Drawing.Font("Microsoft Sans Serif", 16, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
+		self._button2.Location = System.Drawing.Point(200, 355)
+		self._button2.Name = "button2"
+		self._button2.Size = System.Drawing.Size(160, 88)
+		self._button2.TabIndex = 2
+		self._button2.Text = "Clear"
+		self._button2.UseVisualStyleBackColor = True
+		self._button2.Click += self.Button2Click
+		# 
+		# button3
+		# 
+		self._button3.Font = System.Drawing.Font("Microsoft Sans Serif", 16, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
+		self._button3.Location = System.Drawing.Point(384, 355)
+		self._button3.Name = "button3"
+		self._button3.Size = System.Drawing.Size(160, 88)
+		self._button3.TabIndex = 3
+		self._button3.Text = "Exit"
+		self._button3.UseVisualStyleBackColor = True
+		# 
+		# MainForm
+		# 
+		self.BackColor = System.Drawing.SystemColors.ActiveCaption
+		self.ClientSize = System.Drawing.Size(556, 455)
+		self.Controls.Add(self._button3)
+		self.Controls.Add(self._button2)
+		self.Controls.Add(self._button1)
+		self.Controls.Add(self._listBox1)
+		self.Name = "MainForm"
+		self.Text = "Prog162b"
+		self.ResumeLayout(False)
+
+
+	def Button1Click(self, sender, e):
+		heading = "Year\t\tPopulation (in mil.)"
+		self._listBox1.Items.Add(heading)
+		pop = 243
+		for year in range(1990, 2016):
+			line = str(year) + "\t\t" + str(int(pop))
+			self._listBox1.Items.Add(line)
+			pop *= 1.029  # pop = pop * 1.029
+		
+
+	def Button2Click(self, sender, e):
+		self._listBox1.Items.Clear()
