f8e1baaa0be9c5edd51e57911b26e0cbbb6abe9a
Daniel James Szelogowski <dszelogowski@gmail.com>
2024-09-19 15:31:47 -0500

    Update 9/19

---
+++ HelloWorld/MainForm.py
@@ -0,0 +1,89 @@
+﻿import System.Drawing
+import System.Windows.Forms
+
+from System.Drawing import *
+from System.Windows.Forms import *
+
+class MainForm(Form):
+    def __init__(self):
+        self.InitializeComponent()
+    
+    def InitializeComponent(self):
+        self._label1 = System.Windows.Forms.Label()
+        self._button1 = System.Windows.Forms.Button()
+        self._button2 = System.Windows.Forms.Button()
+        self._button3 = System.Windows.Forms.Button()
+        self.SuspendLayout()
+        # 
+        # label1
+        # 
+        self._label1.BackColor = System.Drawing.Color.White
+        self._label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        self._label1.Font = System.Drawing.Font("Fira Code", 17.9999981, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
+        self._label1.Location = System.Drawing.Point(136, 48)
+        self._label1.Name = "label1"
+        self._label1.Size = System.Drawing.Size(592, 272)
+        self._label1.TabIndex = 0
+        self._label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        # 
+        # button1
+        # 
+        self._button1.BackColor = System.Drawing.Color.FromArgb(128, 128, 255)
+        self._button1.Font = System.Drawing.Font("Fira Code", 17.9999981, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, 0)
+        self._button1.ForeColor = System.Drawing.Color.White
+        self._button1.Location = System.Drawing.Point(40, 368)
+        self._button1.Name = "button1"
+        self._button1.Size = System.Drawing.Size(208, 87)
+        self._button1.TabIndex = 1
+        self._button1.Text = "Show"
+        self._button1.UseVisualStyleBackColor = False
+        self._button1.Click += self.Button1Click
+        # 
+        # button2
+        # 
+        self._button2.BackColor = System.Drawing.Color.FromArgb(128, 128, 255)
+        self._button2.Font = System.Drawing.Font("Fira Code", 17.9999981, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, 0)
+        self._button2.ForeColor = System.Drawing.Color.White
+        self._button2.Location = System.Drawing.Point(304, 368)
+        self._button2.Name = "button2"
+        self._button2.Size = System.Drawing.Size(208, 87)
+        self._button2.TabIndex = 2
+        self._button2.Text = "Clear"
+        self._button2.UseVisualStyleBackColor = False
+        self._button2.Click += self.Button2Click
+        # 
+        # button3
+        # 
+        self._button3.BackColor = System.Drawing.Color.FromArgb(128, 128, 255)
+        self._button3.Font = System.Drawing.Font("Fira Code", 17.9999981, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, 0)
+        self._button3.ForeColor = System.Drawing.Color.White
+        self._button3.Location = System.Drawing.Point(568, 368)
+        self._button3.Name = "button3"
+        self._button3.Size = System.Drawing.Size(208, 87)
+        self._button3.TabIndex = 3
+        self._button3.Text = "Exit"
+        self._button3.UseVisualStyleBackColor = False
+        self._button3.Click += self.Button3Click
+        # 
+        # MainForm
+        # 
+        self.BackColor = System.Drawing.Color.FromArgb(192, 192, 255)
+        self.ClientSize = System.Drawing.Size(847, 480)
+        self.Controls.Add(self._button3)
+        self.Controls.Add(self._button2)
+        self.Controls.Add(self._button1)
+        self.Controls.Add(self._label1)
+        self.Name = "MainForm"
+        self.Text = "HelloWorld"
+        self.ResumeLayout(False)
+
+
+    def Button1Click(self, sender, e):
+        self._label1.Text = "Hello, world!"
+
+    def Button2Click(self, sender, e):
+        self._label1.Text = ""
+
+    def Button3Click(self, sender, e):
+        Application.Exit()
+        
