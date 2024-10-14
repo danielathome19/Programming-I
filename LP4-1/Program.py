ac6ead9cb848dc8cd9c359778163ccf57ea78b9c
Daniel James Szelogowski <dszelogowski@gmail.com>
2024-10-14 09:10:57 -0500

    Update 10/14

---
+++ LP4-1/Program.py
@@ -0,0 +1,22 @@
+﻿# For a Python script: New Project > Console Application
+copies = int(input("Enter # of copies to print: "))
+price = 0.0
+cost = 0.0
+
+if copies > 0 and copies <= 99:
+    price = 0.30
+elif copies > 99 and copies <= 499:
+    price = 0.28
+elif copies > 499 and copies <= 749:
+    price = 0.27
+elif copies > 749 and copies <= 1000:
+    price = 0.26
+elif copies > 1000:
+    price = 0.25
+else:
+    print("Invalid # of copies")
+
+cost = copies * price
+print("Price per copy is $" + str(price))
+print("Total cost is $" + str(round(cost,2)))
+input()  # Press 'Enter' to close; only needed for SharpDevelop
