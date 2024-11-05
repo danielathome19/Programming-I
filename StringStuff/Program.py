8f208d84f551f349df79b9bddaf4ddc7df7df251
Daniel James Szelogowski <dszelogowski@gmail.com>
2024-11-05 09:30:26 -0600

    Update 11/5

---
+++ StringStuff/Program.py
@@ -0,0 +1,13 @@
+﻿thing = "HELLO world HERE is SOME text"
+print(thing.lower())
+print(thing.upper())
+print(thing.capitalize())   # Cap. first letter only
+print(thing.title())        # Every word cap.
+print(thing.isdigit())
+print(thing.find("world"))  # Also see rfind, index, and rindex
+print(thing.count("E"))
+print(thing[::-1])          # Reverse
+print(thing.split(" "))
+print("Hello" * 5)
+
+input()
