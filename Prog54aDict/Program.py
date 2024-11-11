ab95f89c7a6b992874fc39ea9844c7e59acdc5d1
Daniel James Szelogowski <dszelogowski@gmail.com>
2024-11-11 09:26:01 -0600

    Update 11/11

---
+++ Prog54aDict/Program.py
@@ -0,0 +1,17 @@
+﻿cars = {
+    "1970 VW Bug":   (286,  9),  # (miles, gallons)
+    "1979 Firebird": (412, 40),
+    "1980 Subaru":   (361, 18),
+    "1975 Cutlass":  (161, 11)
+}
+
+print("Choose a car from the following:", cars.keys())
+mycar = input()
+miles, gallons = cars[mycar]
+mpg = float(miles) / gallons
+
+print("Miles:", miles)
+print("Gallons:", gallons)
+print("MPG:", round(mpg, 1))
+
+input()
---
+++ dictionariesAndTuples/Program.py
@@ -0,0 +1,14 @@
+﻿lst = [1, 2, 3]  # List
+tpl = (1, 2, 3)  # Tuple - immutable, allows unpacking
+a, b, c = tpl    # Unpack tuple into 3 vars
+print(tpl)
+
+dct1 = {1: "apples", 2: "oranges", 3: "pears"}
+dct2 = {"name": "John", "color": "red", 0: [1,2,3]}
+print(dct1[2])
+print(dct2["name"])
+print(dct1)
+print(dct1.keys())
+print(dct1.values())
+
+input()
