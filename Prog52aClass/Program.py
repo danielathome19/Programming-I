b408f254b65f2e897de0b6b82e83a14a1c265740
Daniel James Szelogowski <dszelogowski@gmail.com>
2024-11-12 09:17:40 -0600

    Update 11/12

---
+++ Prog52aClass/Program.py
@@ -0,0 +1,21 @@
+﻿class Rectangle:
+    def __init__(self, length, width):  # Constructor
+        self.length = length
+        self.width  = width
+        
+    def area(self):  # Class function
+        return self.length * self.width
+    
+    def perimeter(self):
+        return 2 * self.length + 2 * self.width
+    
+    
+length = int(input("Enter length: "))
+width = int(input("Enter width: "))
+r = Rectangle(length, width)  # Call class constructor to make new instance
+print("Area:", r.area())
+print("Perimeter:", r.perimeter())
+r.length *= 2
+print("Length * 2:", r.length)
+
+input()
